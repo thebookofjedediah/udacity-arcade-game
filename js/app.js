82158d2819cd1ba54092114ef07a090d9d67f91a
Jedediah Arnold <jarnold910@gmail.com>
2019-09-27 14:34:06 -0400

    6 bugs that move in their rows

--- js/app.js
+++ js/app.js
@@ -1,11 +1,13 @@
 // Enemies our player must avoid
-var Enemy = function(x,y) {
+var Enemy = function(x,y,speed) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
     // According to engine.js x=101 and y=83
     this.x = x;
     this.y = y;
+    this.speed = speed;
+
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
@@ -17,10 +19,16 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+
+      this.x += this.speed * dt;
+
 };
-let enemy1 = new Enemy(0,60);
-let enemy2 = new Enemy(0,143);
-let enemy3 = new Enemy(0,226);
+let enemy1 = new Enemy(0,60,100);
+let enemy2 = new Enemy(0,143,100);
+let enemy3 = new Enemy(0,226,100);
+let enemy4 = new Enemy(0,60,150);
+let enemy5 = new Enemy(0,143,200);
+let enemy6 = new Enemy(0,226,250);
 // Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
@@ -33,7 +41,7 @@ Enemy.prototype.render = function() {
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
-const allEnemies = [enemy1,enemy2,enemy3]
+const allEnemies = [enemy1,enemy2,enemy3,enemy4,enemy5,enemy6]
 // Place the player object in a variable called player
 
 
