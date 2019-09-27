5105c595e90766ede5a5e67bc4e4b5dc409da6bc
Jedediah Arnold <jarnold910@gmail.com>
2019-09-27 14:41:46 -0400

    6 bugs reappear when reaching the end of canvas

--- js/app.js
+++ js/app.js
@@ -19,8 +19,12 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
-
+    if (this.x < 504){
       this.x += this.speed * dt;
+    } else {
+      this.x = -101;
+    }
+
 
 };
 let enemy1 = new Enemy(0,60,100);
