a3dfa653b26a5b5ed27ca60a3cc8f21c18613ceb
Jedediah Arnold <jarnold910@gmail.com>
2019-09-27 14:48:13 -0400

    hero is rendered in starting line, no functionality yet

--- js/app.js
+++ js/app.js
@@ -41,13 +41,22 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
-
+class Player {
+  constructor(){
+    this.sprite = "images/char-boy.png";
+    this.x = 0;
+    this.y = 392;
+  }
+  update(){}
+  render(){ctx.drawImage(Resources.get(this.sprite), this.x, this.y);}
+  handleInput(){}
+}
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 const allEnemies = [enemy1,enemy2,enemy3,enemy4,enemy5,enemy6]
 // Place the player object in a variable called player
-
+const player = new Player();
 
 
 // This listens for key presses and sends the keys to your
